fc2b18e99dc448f46883641f9f81d7d7bdc1868d
Nicole Freed <nicole.freed@gmail.com>
2017-08-21 14:26:07 -0700

    import and add housing component to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
+import { HousingComponent } from './housing/housing.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: CategoriesComponent
+  },
+  {
+    path: 'housing',
+    component: HousingComponent
   }
 ];
 
