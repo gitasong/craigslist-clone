cbf1d9b465e652714315e3b199b16b3f3a317cc1
Nicole Freed <nicole.freed@gmail.com>
2017-08-21 13:51:54 -0700

    import and add CategoriesComponent to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: CategoriesComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
